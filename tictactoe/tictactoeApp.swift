8b3066513eabd20ff32b9c65b8eee50b2253b28c
William <s3864077@rmit.edu.vn>
2022-08-12 19:03:40 +0700

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by William on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
